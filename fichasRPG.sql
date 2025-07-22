cb4405200d31d3e555e6d023bbf6dfb2c1430ea1
Artur Tome <arturtomex@gmail.com>
2025-07-22 08:49:54 -0300

    Update banco de dados fichasRPG.sql

--- fichasRPG.sql
+++ fichasRPG.sql
@@ -20,3 +20,21 @@ insert into elementos (escuridao, 0.75, 1, 1, 1, 2, 2, 0.75);
 insert into elementos (luz, 1.5, 1, 1, 1, 2, 1, 1);
 insert into elementos (eletricidade, 1.25, 2.5, 0.25, 0, 1.5, 1.5, 1)
 
+create table clase(
+classeAtacante varchar(12)primary key,
+saber float,
+archer float,
+lancer float,
+rider float,
+assassin float,
+berserker float,
+caster float
+);
+
+insert into clase (saber, 1, 0.9, 1.1, 1, 1.1, 2, 1.1);
+insert into clase (archer, 1.1, 1, 0.9, 1.1, 1, 2, 1);
+insert into clase (lancer, 0.9, 1, 1, 1, 0.9, 2, 1.1);
+insert into clase (rider, 1, 1.1, 1, 1, 1.1, 2, 0.9);
+insert into clase (assassin, 1.1, 0.9, 1.1, 1, 1, 2, 1.1);
+insert into clase (berserker, 2, 2, 2, 2, 2, 2, 2);
+insert into clase (caster, 1, 1.1, 1, 0.9, 1, 2, 1);
