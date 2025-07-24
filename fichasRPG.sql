b866ad88e3d3caf5aecbd72a94f721847ebbe34a
Artur Tome <arturtomex@gmail.com>
2025-07-23 22:26:03 -0300

    Update BD 3 fichasRPG.sql

--- fichasRPG.sql
+++ fichasRPG.sql
@@ -2,7 +2,8 @@ create schema fichas;
 use fichas;
 
 create table elementos(
-elementoAtacante varchar(12) primary key,
+idElementos int primary key,
+elementoAtacante varchar(12) unique key,
 fogo float,
 agua float,
 vento float,
@@ -13,17 +14,18 @@ eletricidade float
 );
 
 insert into elementos values
-(fogo, 1, 0.5, 2, 0.75, 1.5, 1.5, 1.5),
-(agua, 2, 0.75, 1.25, 1.75, 1, 1, 2),
-(vento, 2, 0.75, 1, 0.75, 1, 1, 0.5),
-(terra, 2, 0.5, 0.75, 1, 1, 1, 2.5),
-(escuridao, 0.75, 1, 1, 1, 2, 2, 0.75),
-(luz, 1.5, 1, 1, 1, 2, 1, 1),
-(eletricidade, 1.25, 2.5, 0.25, 0, 1.5, 1.5, 1)
+(1, 'fogo', 1, 0.5, 2, 0.75, 1.5, 1.5, 1.5),
+(2, 'agua', 2, 0.75, 1.25, 1.75, 1, 1, 2),
+(3, 'vento', 2, 0.75, 1, 0.75, 1, 1, 0.5),
+(4, 'terra', 2, 0.5, 0.75, 1, 1, 1, 2.5),
+(5, 'escuridao', 0.75, 1, 1, 1, 2, 2, 0.75),
+(6, 'luz', 1.5, 1, 1, 1, 2, 1, 1),
+(7, 'eletricidade', 1.25, 2.5, 0.25, 0, 1.5, 1.5, 1)
 ;
 
-create table clase(
-classeAtacante varchar(12)primary key,
+create table classe(
+idClasse int primary key,
+classeAtacante varchar(12) unique key,
 saber float,
 archer float,
 lancer float,
@@ -33,13 +35,14 @@ berserker float,
 caster float
 );
 
-insert into classe values (saber, 1, 0.9, 1.1, 1, 1.1, 2, 1.1),
-(archer, 1.1, 1, 0.9, 1.1, 1, 2, 1),
-(lancer, 0.9, 1, 1, 1, 0.9, 2, 1.1),
-(rider, 1, 1.1, 1, 1, 1.1, 2, 0.9),
-(assassin, 1.1, 0.9, 1.1, 1, 1, 2, 1.1),
-(berserker, 2, 2, 2, 2, 2, 2, 2),
-(caster, 1, 1.1, 1, 0.9, 1, 2, 1)
+insert into classe values 
+(1,'saber', 1, 0.9, 1.1, 1, 1.1, 2, 1.1),
+(2,'archer', 1.1, 1, 0.9, 1.1, 1, 2, 1),
+(3,'lancer', 0.9, 1, 1, 1, 0.9, 2, 1.1),
+(4,'rider', 1, 1.1, 1, 1, 1.1, 2, 0.9),
+(5,'assassin', 1.1, 0.9, 1.1, 1, 1, 2, 1.1),
+(6,'berserker', 2, 2, 2, 2, 2, 2, 2),
+(7,'caster', 1, 1.1, 1, 0.9, 1, 2, 1)
 ;
 
 create table status(
@@ -65,4 +68,4 @@ velocidade int not null,
 destreza int not null,
 estamina int not null,
 mana int not null
-)
+);
